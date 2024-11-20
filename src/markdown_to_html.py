e8ccd0a3f727c0669f92c913112d475ed8c97fc9
favreje <favreje@gmail.com>
2024-11-20 17:09:36 -0600

    partial markdown_to_html_node func

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,5 +1,6 @@
 from enum import Enum, auto
 import re
+from htmlnode import HTMLNode
 
 
 class BlockType(Enum):
@@ -41,17 +42,68 @@ def block_to_block_type(block):
     return BlockType.PARAGRAPH
 
 
+def get_heading_level(block):
+    match = re.match(r"(#+)\s(.+?)\s*$", block)
+    if match:
+        hashes, header_text = match.groups()
+        if not header_text.strip():
+            raise ValueError("Header must contain text")
+        level = str(len(hashes))
+        header = "h" + level
+        header_text = header_text.strip()
+        return header, header_text
+    return None, None
+
+
+def parse_markdown_list_items(block_type, block):
+    if block_type not in (BlockType.ORDERED_LIST, BlockType.UNORDERED_LIST):
+        raise ValueError("BlockType must be ORDERED_LIST or UNORDERED_LIST.")
+    children = []
+    pattern = r"^[*-]\s+(.+)$" if block_type == BlockType.UNORDERED_LIST else r"^[0-9]+\.\s+(.+)$"
+    match = re.finditer(pattern, block, re.MULTILINE)
+    for m in match:
+        li = m.group(1)
+        children.append(HTMLNode(tag="li", value=li, children=None, props=None))
+    return children
+
+
+def block_to_node(block):
+    block_type = block_to_block_type(block)
+    if block_type == BlockType.PARAGRAPH:
+        return HTMLNode(tag="p", value=block, children=None, props=None)
+    if block_type == BlockType.HEADING:
+        header, header_text = get_heading_level(block)
+        return HTMLNode(tag=header, value=header_text, children=None, props=None)
+    if block_type == BlockType.CODE:
+        match = re.match(r"^```([\s\S]*?)```(?=\Z)", block)
+        if match:
+            code_text = match.groups()[0].strip()
+        else:
+            code_text = None
+        child = HTMLNode(tag="code", value=code_text, children=None, props=None)
+        return HTMLNode(tag="pre", value=None, children=[child], props=None)
+    if block_type == BlockType.QUOTE:
+        lines = re.findall(r"^>\s?(.*)", block, re.MULTILINE)
+        text = "\n".join(lines)
+        return HTMLNode(tag="blockquote", value=text, children=None, props=None)
+    if block_type == BlockType.UNORDERED_LIST:
+        children = parse_markdown_list_items(BlockType.UNORDERED_LIST, block)
+        return HTMLNode(tag="ul", value=None, children=children, props=None)
+    if block_type == BlockType.ORDERED_LIST:
+        children = parse_markdown_list_items(BlockType.ORDERED_LIST, block)
+        return HTMLNode(tag="ol", value=None, children=children, props=None)
+
+
 def markdown_to_html_node(markdown):
+    node_list = []
     blocks = markdown_to_blocks(markdown)
-    block_types = []
     for block in blocks:
-        block_type = block_to_block_type(block)
-        block_types.append(block_type)
-    return block_types, blocks
+        node = block_to_node(block)
+        node_list.append(node)
+    return HTMLNode(tag="div", value=None, children=node_list, props=None)
 
 
 def main():
-    # ---- Preliminary Tests ----- #
     markdown_test = """
 # Blog Post
 
@@ -61,54 +113,36 @@ The main paragraph of my blog post will discuss the following points:
 2. Point number two
 3. Point number three
 
+And these unordered list items
+
+- First random thing
+- Second random thing
+- Third random thing
+- Fourth random thing (in no particular order)
+
 And it will contain the following code snippet:
 
 ```
-print("Hello world")
+for i in range(10):
+    print("Hello world")
 ```
+
+> Words can be like X-rays,
+> if you use them properly—they’ll go through anything.
+> You read and you’re pierced.
 
 ## Second Point:
 
 Markdown can contain **various** in-line *code*
 
 
-        """
-    all_tests = [
-        "###### My Header",
-        "# Jeff's Code",
-        "####### Too long to be a header",
-        "Words before ## This Header",
-        "## A sub-header ##",
-        "#Not a header because no space",
-        "```# An inline block of code that is commented ```, followed by a **bold** word.",
-        "Preceded text ```# An inline block of code because of preceeding text.```",
-        "```# A true code block that is commented\n It also has\n multiple lines```",
-        "```\nA true code block that is commented\n It also has\n multiple lines\n```",
-        "> 'This would be quoted text.'",
-        ">What if no space?",
-        "> 'This would also be quoted text.\n> Because each line has the quote character'\n",
-        "> 'This would not.\nBecause it needs the proper character on each line.'",
-        "* First item\n* Second item",
-        "* First item\n* Second item\n Tricky third item - it's a cheeky monkey",
-        "* First item\n* Second item\n- Tricky third item - it's a cheeky monkey",
-    ]
+"""
 
-    test_blocks = [
-        "01. item\n02. item",
-        "1. First item\n2. Second item\n Tricky third item - it's a cheeky monkey",
-        "1. fixed it\n2. First item\n3. Second item\n4. Tricky because the list doesn't start with 1.",
-        "```\nA true code block that is commented\n It also has\n multiple lines\n```",
-    ]
-
-    # for block in test_blocks:
-    #     print(f"{block}\n{block_to_block_type(block)}\n")
-    #
-    # for block in all_tests:
-    #     print(f"{block}\n{block_to_block_type(block)}\n")
-
-    block_types, blocks = markdown_to_html_node(markdown_test)
-    for i in range(len(block_types)):
-        print(f"{i + 1}:  {block_types[i]}\n{blocks[i]}\n")
+    main_node = markdown_to_html_node(markdown_test)
+    print(main_node)
+    print()
+    for child in main_node.children:
+        print(child)
 
 
 if __name__ == "__main__":
