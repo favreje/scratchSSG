9f83238cb50f2a1a6ca1cfdec4d2081500a35fc2
favreje <favreje@gmail.com>
2024-12-06 13:38:18 -0600

    added markdown_to_html_node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -44,6 +44,9 @@ class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
 
+    def __repr__(self):
+        return f"LeafNode({self.tag=}, {self.value=}, {self.props=})"
+
     def to_html(self):
         if self.value == None:
             raise ValueError("All leaf nodes must have a value.")
--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,6 +1,8 @@
 from enum import Enum, auto
 import re
-from htmlnode import HTMLNode
+from htmlnode import ParentNode, HTMLNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
 
 
 class BlockType(Enum):
@@ -58,34 +60,49 @@ def get_heading_level(block):
 def parse_markdown_list_items(block_type, block):
     if block_type not in (BlockType.ORDERED_LIST, BlockType.UNORDERED_LIST):
         raise ValueError("BlockType must be ORDERED_LIST or UNORDERED_LIST.")
-    children = []
+    list_items = []
     pattern = r"^[*-]\s+(.+)$" if block_type == BlockType.UNORDERED_LIST else r"^[0-9]+\.\s+(.+)$"
     match = re.finditer(pattern, block, re.MULTILINE)
     for m in match:
-        li = m.group(1)
-        children.append(HTMLNode(tag="li", value=li, children=None, props=None))
+        text = m.group(1)
+        children = text_to_children(text)
+        list_items.append(ParentNode(tag="li", children=children))
+    return list_items
+
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = [text_node_to_html_node(child) for child in text_nodes]
     return children
 
 
 def block_to_node(block):
     block_type = block_to_block_type(block)
     if block_type == BlockType.PARAGRAPH:
-        return HTMLNode(tag="p", value=block, children=None, props=None)
+        children = text_to_children(block)
+        node = ParentNode(tag="p", children=children)
+        return node
     if block_type == BlockType.HEADING:
         header, header_text = get_heading_level(block)
-        return HTMLNode(tag=header, value=header_text, children=None, props=None)
+        children = text_to_children(header_text)
+        node = ParentNode(tag=header, children=children)
+        return node
     if block_type == BlockType.CODE:
         match = re.match(r"^```([\s\S]*?)```(?=\Z)", block)
         if match:
             code_text = match.groups()[0].strip()
         else:
             code_text = None
-        child = HTMLNode(tag="code", value=code_text, children=None, props=None)
-        return HTMLNode(tag="pre", value=None, children=[child], props=None)
+        children = text_to_children(code_text)
+        code_child = ParentNode(tag="code", children=children)
+        node = ParentNode(tag="pre", children=code_child)
+        return node
     if block_type == BlockType.QUOTE:
         lines = re.findall(r"^>\s?(.*)", block, re.MULTILINE)
         text = "\n".join(lines)
-        return HTMLNode(tag="blockquote", value=text, children=None, props=None)
+        children = text_to_children(text)
+        node = ParentNode(tag="blockquote", children=children)
+        return node
     if block_type == BlockType.UNORDERED_LIST:
         children = parse_markdown_list_items(BlockType.UNORDERED_LIST, block)
         return HTMLNode(tag="ul", value=None, children=children, props=None)
@@ -100,11 +117,11 @@ def markdown_to_html_node(markdown):
     for block in blocks:
         node = block_to_node(block)
         node_list.append(node)
-    return HTMLNode(tag="div", value=None, children=node_list, props=None)
+    return ParentNode(tag="div", children=node_list)
 
 
 def main():
-    markdown_test = """
+    markdown_text = """
 # Blog Post
 
 The main paragraph of my blog post will discuss the following points:
@@ -118,7 +135,7 @@ And these unordered list items
 - First random thing
 - Second random thing
 - Third random thing
-- Fourth random thing (in no particular order)
+- Fourth random thing **in no particular order**
 
 And it will contain the following code snippet:
 
@@ -128,22 +145,37 @@ for i in range(10):
 ```
 
 > Words can be like X-rays,
-> if you use them properly—they’ll go through anything.
-> You read and you’re pierced.
+> if you use them properly—they’ll go through *anything*.
+> You read and you’re *pierced*.
 
 ## Second Point:
 
-Markdown can contain **various** in-line *code*
+### Markdown can contain **various** in-line *code*, links and images
+
+#### Here are a few useful links:
+
+[boot-dot-dev](https://www.youtube.com/@bootdotdev)
+[My home page](https://www.favreje.com)
+
+#### And some funny images
+
+![Rick has no F's to give](https://i.imgur.com/aKaOqIh.gif)
+
 
 
 """
 
-    main_node = markdown_to_html_node(markdown_test)
-    print(main_node)
-    print()
+    main_node = markdown_to_html_node(markdown_text)
     for child in main_node.children:
-        print(child)
+        print(child.tag)
+        print(child.children)
+        print()
+
+    print("----------")
+    print(main_node)
 
 
+#
+#
 if __name__ == "__main__":
     main()
